f47108ece298498ac9c45f6e326bf429390a5d1a
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-02-19 00:23:22 -0600

    added basic click alert function

---
+++ index.js
@@ -0,0 +1,7 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    alert("I got clicked!");
+
+  });
+}
